e021115503773886f31454a16de12d2f13823eb7
khaled975 <khaledbebo975@gmail.com>
2023-06-06 03:47:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useLayoutEffect,useState} from "react"
+import {Table} from 'antd';
+import { HashLoader } from 'react-spinners';
+function App () {
+const { Column} = Table;
+const [users,setUsers]=useState([])
+const [loading,setLoading]=useState(false)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+///////// fetch data
+useLayoutEffect(()=>{
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res)=>res.json())
+  .then((res)=>setUsers(res))
+},[])
+
+/////////// loading
+useLayoutEffect(()=>{
+  setLoading(true)
+  setTimeout(()=>setLoading(false),3000)
+},[])
+
+return(
+  <div className='App'>
+  {
+  loading?
+    <HashLoader color="#36d7b7" size={120}/>
+    :
+    <div className='box'>
+      <Table dataSource={users} >
+        <Column title="First Name" dataIndex="name" key="name" />
+        <Column title="Age" dataIndex='id' key="id" />
+        <Column title="Address" dataIndex='username' key="username" />
+      </Table>
     </div>
-  );
+    }
+  </div>
+);
 }
-
 export default App;
